31cc90bd4f27cc907a1752045ee3533245a5d59e
Idris <idrisakande2545@gmail.com>
2024-03-20 05:46:30 +0100

    adjument of mobile styling at portfolio secton

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { useMediaQuery } from "../../ReuseComponents/hooks/useMediaQuery";
 
 export const ProjectCard = ({ project }) => {
   const {
@@ -15,9 +16,11 @@ export const ProjectCard = ({ project }) => {
     gitHub,
     tag,
   } = project;
+
+  const isAboveMediumScreens = useMediaQuery("(min-width: 768px)");
   return (
     // Project section
-    <motion.section
+    <>{isAboveMediumScreens ? <motion.section
       whileInView={{ scale: [0, 1] }}
       transition={{ duration: 1, ease: "easeInOut" }}
       className="bg-white flex flex-col gap-4 p-3 w-56 rounded-lg cursor-pointer xs:w-64"
@@ -73,6 +76,50 @@ export const ProjectCard = ({ project }) => {
           </p>
         </div>
       </div>
-    </motion.section>
+    </motion.section> : <motion.section
+      whileInView={{ scale: [0, 1] }}
+      transition={{ duration: 1, ease: "easeInOut" }}
+      className="bg-white flex flex-col gap-4 p-3 w-56 rounded-lg cursor-pointer xs:w-64"
+    >
+      <div className="relative overflow-hidden">
+        <div className={divStyle} />
+        <img className="h-44 rounded-lg xs:h-52" src={img1} alt={name} />
+        {img2 && <img className={img2Style} src={img2} alt={name} />}
+
+        <motion.div
+          className={`flex gap-2 justify-center items-center absolute top-0 left-0 bottom-0 right-0 rounded-lg ${isAboveMediumScreens ? `opacity-0 hover:bg-black hover:bg-opacity-40` : `opacity-70 bg-black/40`}`}
+        >
+          {url && (
+            <a href={url} target="_blank" rel="noreferrer">
+              <motion.div
+                className="bg-black flex justify-center items-center w-10 h-10 rounded-full bg-opacity-80"
+              >
+                <AiFillEye className="text-lg text-white" />
+              </motion.div>
+            </a>
+          )}
+          {gitHub && (
+            <a href={gitHub} target="_blank" rel="noreferrer">
+              <motion.div
+                className="bg-black flex justify-center items-center w-10 h-10 rounded-full bg-opacity-80"
+              >
+                <AiFillGithub className="text-lg text-white" />
+              </motion.div>
+            </a>
+          )}
+        </motion.div>
+      </div>
+
+      <div className="font-bold relative flex flex-col gap-1 xs:gap-2">
+        <h4 className="text-sm">{title}</h4>
+        <p className="text-xs text-gray">{discription}</p>
+
+        <div className="flex justify-center items-center absolute -top-9 left-0 right-0">
+          <p className="bg-white text-xs text-gray py-1 px-2 rounded-md">
+            {tag}
+          </p>
+        </div>
+      </div>
+    </motion.section>}</>
   );
 };
